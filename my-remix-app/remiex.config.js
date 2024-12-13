0447cd65a372e806ca53fb09c550b960628349a2
DPontaro <n.douzono@gmail.com>
2024-12-13 22:38:07 +0900

    add remix config

---
+++ my-remix-app/remiex.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+    appDirectory: "app",
+    assetsBuildDirectory: "public/build", // 静的ファイルのビルド先
+    publicPath: "/build/",
+    serverBuildPath: "build/server/index.js", // サーバービルドの出力先
+  };
+  
