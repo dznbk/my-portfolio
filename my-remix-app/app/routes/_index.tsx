4e0899778963d8046b940d6865abc462d85a58bf
DPontaro <n.douzono@gmail.com>
2024-12-13 22:14:28 +0900

    各種ロゴ追加

--- my-remix-app/app/routes/_index.tsx
+++ my-remix-app/app/routes/_index.tsx
@@ -21,49 +21,73 @@ export default function Index() {
         <p className="text-sm text-gray-600 dark:text-gray-300 text-center">
           エンジニア | Webアプリ開発者
         </p>
-        <nav className="flex flex-col items-center justify-center gap-4">
-          <ul className="flex flex-col gap-2">
-            <li>
-              <a
-                className="text-blue-700 hover:underline dark:text-blue-500"
-                href="https://x.com/DPontaro"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Twitter
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-blue-700 hover:underline dark:text-blue-500"
-                href="https://github.com/dznbk"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-blue-700 hover:underline dark:text-blue-500"
-                href="https://zenn.dev/d_pontaro"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Zenn
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-blue-700 hover:underline dark:text-blue-500"
-                href="https://note.com/ponpon63"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                note
-              </a>
-            </li>
-          </ul>
+        <nav className="flex justify-center gap-6">
+          {/* X (旧Twitter) */}
+          <a
+            href="https://x.com/DPontaro"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/icons/x-logo.svg"
+              alt="X Logo"
+              className="w-8 h-8"
+            />
+          </a>
+
+          {/* GitHub */}
+          <a
+            href="https://github.com/dznbk"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/icons/github-mark.svg"
+              alt="GitHub Logo"
+              className="w-8 h-8 dark:hidden"
+            />
+            <img
+              src="/icons/github-mark-white.svg"
+              alt="GitHub Logo (Dark)"
+              className="w-8 h-8 hidden dark:block"
+            />
+          </a>
+
+          {/* Zenn */}
+          <a
+            href="https://zenn.dev/d_pontaro"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/icons/zenn-logo.svg"
+              alt="Zenn Logo"
+              className="w-8 h-8 dark:hidden"
+            />
+            <img
+              src="/icons/zenn-logo-white.svg"
+              alt="Zenn Logo (Dark)"
+              className="w-8 h-8 hidden dark:block"
+            />
+          </a>
+
+          {/* Note */}
+          <a
+            href="https://note.com/ponpon63"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/icons/note-logo.svg"
+              alt="Note Logo"
+              className="w-8 h-8 dark:hidden"
+            />
+            <img
+              src="/icons/note-logo-white.svg"
+              alt="Note Logo (Dark)"
+              className="w-8 h-8 hidden dark:block"
+            />
+          </a>
         </nav>
       </div>
   );
